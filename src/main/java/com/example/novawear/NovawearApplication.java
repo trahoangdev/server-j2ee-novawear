00f2310c5a6cf9c2f9b4204cf5f14292ab157e4e
trahoangdev <trahoangdev@gmail.com>
2026-02-04 02:31:39 +0700

    Initial commit: NovaWear J2EE Spring Boot application

---
+++ src/main/java/com/example/novawear/NovawearApplication.java
@@ -0,0 +1,13 @@
+package com.example.novawear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovawearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovawearApplication.class, args);
+	}
+
+}
